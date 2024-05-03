7f1c9b96b662f7e02ce5aaf554fb02325d2a0cdb
LINDAMND2 <167573951+LINDAMND2@users.noreply.github.com>
2024-05-03 10:57:57 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,7 @@ if ingredient_list:
   #  st.write(ingredient_list)
  #   st.text(ingredient_list)
     ingredients_string = ''
-    for fruit_chosen in ingredient_list:
+    for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
         st.subheader(fruit_chosen + ' Nutrition Information')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
