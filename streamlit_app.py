07ba1535625d9e4191ebcda9512a5e28428cf553
LINDAMND2 <167573951+LINDAMND2@users.noreply.github.com>
2024-05-03 10:15:08 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,9 @@ st.write(
 )
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response.json())
+#st.text(fruityvice_response.json())
+
+fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
 name_on_order = st.text_input('Name on Smoothie')
 st.write('The name on your smoothies will be ', name_on_order)
