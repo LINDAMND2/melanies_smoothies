fedb25125c12bb86372819d7262720c1210db03f
LINDAMND2 <167573951+LINDAMND2@users.noreply.github.com>
2024-05-03 11:34:55 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 
 import streamlit as st
-from snowflake.snowpark.functions import col
 import requests
 import pandas as pd
+from snowflake.snowpark.functions import col
 
 # Write directly to the app
 st.title("Customize your smoothie")
