91bffcf760cf47062d7935086a5f2ecaadbebd20
LINDAMND2 <167573951+LINDAMND2@users.noreply.github.com>
2024-05-03 10:22:37 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,7 +34,7 @@ if ingredient_list and len(ingredient_list) < 6:
     for fruit_chosen in ingredient_list:
         ingredients_string += fruit_chosen + ' '
         st.subheader(fruit_chosen + 'Nutrition Information')
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon" + fruit_chosen)
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
     st.write(ingredients_string)
     my_insert_statement = "insert into SMOOTHIES.PUBLIC.ORDERS(INGREDIENTS, NAME_ON_ORDER) values('"+ingredients_string+"','"+name_on_order+"');"
